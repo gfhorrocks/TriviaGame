397be6b3783383062b3cbf45e9f30e5d3449b1f6
GregH <43968370+gfhorrocks@users.noreply.github.com>
2019-03-07 20:59:36 -0500

    Updated Game Object Questions and Answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+var game {
+    question: ["What does PC stand for?",
+    'Who originated the concept of a programmable computer and is considered to be the "Father of the Computer"?',
+    "This computer used more than 300 vacuum tubes for digital computation, including binary math and boolean logic.",
+    "This 4-bit central processing unit (CPU) was the first commercially available microprocessor released by Intel.",
+    "This was the first graphics processing unit (GPU), which was capable of processing a minimum of 10 million polygons per second.",
+    "On this date in history, ARPANET adopted TCP/IP which became the modern Internet.",
+    "This game was the first of it's kind to use the TCP/IP protocol.",
+    "This American microprocessor maker is Intel's biggest compeitor and released the Athlon Processor in year 2000.",
+    "In the early 2000s, these two strings were the most commonly used passwords.",
+    "The first known computer virus was written in 1981 on this computer."],
+
+    answer: ["personal computer","charles babbage","abc","intel 4004","geforce 256","january 1 1983","sgi dogfight","password, 123456", "apple ii"],
+
+    
+}
