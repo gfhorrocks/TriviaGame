2ed1f112a199b16831ef3757f4fbf004a37af5f2
GregH <43968370+gfhorrocks@users.noreply.github.com>
2019-03-10 23:41:00 -0400

    Fixed bugs and responsiveness

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,16 +33,16 @@ var game = {
     clockRunning: false,
 
     //Answers all in order.  Calling them will start at question * 4
-    answers: ["politically correct", "personal computer", "point & click", "personal computator",
-        "charles babbage", "john backus", "david bader", "charles bachman",
-        "mcom", "xyz", "abc", "zfz",
-        "amd athlon", "intel i2", "ibm core", "intel 4004",
-        "amd radeon", "geforce 256", "intel igx", "nvidia quadro",
-        "january 1 1983", "february 9 1980", "october 31 1983", "december 5 1981",
-        "tetris", "dig dug", "sgi dogfight", "dungeons and dragons",
-        "amd", "gfx", "ibm", "sun",
-        "eggs, ham", "password, 123456", "letmein, pleaseletmein", "abcdefg, 987654",
-        "ibm xt", "sun solaris", "altair 8800", "apple ii"],
+    answers: ["Politically Correct", "Personal Computer", "Point & Click", "Personal Computator",
+        "Charles babbage", "John backus", "David bader", "Charles bachman",
+        "Mcom", "XYZ", "ABC", "ZFZ",
+        "AMD Athlon", "Intel i2", "IBM Core", "Intel 4004",
+        "AMD Radeon", "Geforce 256", "Intel IGX", "Nvidia Quadro",
+        "January 1 1983", "February 9 1980", "October 31 1983", "December 5 1981",
+        "Tetris", "Dig Dug", "SGI Dogfight", "Dungeons and Dragons",
+        "AMD", "GFX", "IBM", "Sun",
+        "eggs and ham", "password and 123456", "letmein and pleaseletmein", "abcdefg and 987654",
+        "IBM XT", "Sun Solaris", "Altair 8800", "Apple ii"],
 
     startTimer: function () {                                //STARTS TIMER
         if (!game.clockRunning) {
@@ -61,20 +61,17 @@ var game = {
         time--;
 
         if (time === -1) {
-            game.endGame();
+            game.endGame();                                  //When timer hits below 0, end game
         }
-
     },
 
-    drawData: function() {
+    drawData: function () {
 
         var index = 0;
-
         //Updates screen with current question and clears the choices Div
-        $("#questionDiv").text(game.question[q]); //Updates Question
-        $("#choicesDiv").empty();                 //Empties answers
-        $("#startButton").hide();                 //Hides the start button
-
+        $("#questionDiv").text(game.question[q]);           //Updates Question
+        $("#choicesDiv").empty();                           //Empties answers
+        $("#startButton").hide();                           //Hides the start button
 
         //Starts at the cooresponding answer depending on what question you're on. Ex: Qestion 1 (0-3) Question 2 (4-7)... Adds new Div so it can be highlighted and clicked on
         for (var i = q * 4; i < (q * 4) + 4; i++) {
@@ -83,12 +80,13 @@ var game = {
             newDiv.attr("class", "answer");
             newDiv.attr("choiceIndex", index);
             $("#choicesDiv").append(newDiv);
-            index++;                              //Index for simply adding 0,1,2 or 3 to the newDiv attr
+            index++;                                         //Index for simply adding 0,1,2 or 3 to the newDiv attr
         }
         game.startTimer();
     },
 
     drawAnswer: function (result) {
+
         $("#questionDiv").empty();                           //Clears question
         $("#choicesDiv").empty();                            //Clears choices
 
@@ -104,8 +102,8 @@ var game = {
                 break;
             }
         }
-
-        $("#choicesDiv").html("<img src='assets/images/" + game.imagePath[q] + "'/>");   //Output image of answer
+        //Output image of answer
+        $("#choicesDiv").html("<img src='assets/images/" + game.imagePath[q] + "'/>");
     },
 
     //Checks the correctAnswer string to see if it matches the choiceIndex of what you're clicking on
@@ -114,19 +112,19 @@ var game = {
         if ($(this).attr("choiceIndex") == game.correctAnswer[q]) {
             correct++;
             game.drawAnswer(true);                           //Draws the answer on the screen
-            setTimeout(game.drawData, 5000);                  //Waits 7 seconds before drawing new question
         }
         else {
             game.drawAnswer(false);
-            setTimeout(game.drawData, 5000);
         }
 
-        q++;
+        q++;                                                 //Goes to next Question index
 
-        if(q===10){
+        if (q === game.question.length) {
             game.endGame();
         }
-
+        else {
+            setTimeout(game.drawData, 7000);                 //Waits 7 seconds before drawing new question
+        }
     },
 
     endGame: function () {
@@ -139,17 +137,17 @@ var game = {
         $("#timerDiv").empty();
 
         if (time === -1) {
-            $("#questionDiv").text("Sorry, but you ran out of time, better luck Next Time!");
+            $("#questionDiv").text("Sorry, but you ran out of time! Better luck Next Time!");
         }
         else {
-            $("#questionDiv").text("Game complete! " + "You answered " +correct + " out of 10 questions correctly!");
+            $("#questionDiv").text("Game complete! " + "You answered " + correct + " out of 10 questions correctly!");
         }
 
-        correct = 0;    //resetting values for new game
+        correct = 0;                                         //resetting values for new game
         time = 60;
         q = 0;
     }
 };
 
-$(document).on("click", ".answer", game.checkifCorrect);           //When answer selection is clicked, first check to see if it's correct
-$(document).on("click", "#startButton", game.drawData);            //When Start button is clicked, start the game with drawData function
+$(document).on("click", ".answer", game.checkifCorrect);     //When answer selection is clicked, first check to see if it's correct
+$(document).on("click", "#startButton", game.drawData);      //When Start button is clicked, start the game with drawData function
